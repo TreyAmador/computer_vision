690de5be6f8b138d403d3603e1e51295a89342fc
Trey Amador <tmamador@cpp.edu>
2017-10-19 00:05:54 -0700

    add angle rounding

--- canny.py
+++ canny.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 from PIL import Image
 import numpy as np
 import math
+import time
 import sys
 
 
@@ -11,6 +12,30 @@ def init_img(filepath):
     img = np.asarray(Image.open(filepath).convert('L'))
     pixels = np.array([[col for col in row] for row in img])
     return pixels
+
+
+def gen_kernels():
+    kx = [[1,0,-1],[2,0,-2],[1,0,-1]]
+    ky = [[1,2,1],[0,0,0],[-1,-2,-1]]
+    return kx,ky
+
+
+def round_angle(rad):
+    deg = (180/math.pi)*rad
+    if 0 <= deg < 45:
+        pass
+        #print(deg,end=' ')
+    elif 45 <= deg < 90:
+        pass
+        #print(deg,end=' ')
+    elif 90 <= deg < 135:
+        pass
+        #print(deg,end=' ')
+    elif 135 <= deg < 180:
+        print(deg,end=' ')
+    else:
+        print('\n\nnot in value\n')
+    return rad
 
 
 def gaussian_smooth(img,dim):
@@ -26,6 +51,24 @@ def gaussian_smooth(img,dim):
     return fltrd
 
 
+# this works, but is not being using
+def sobel_filter(img):
+    dx = deepcopy(img)
+    dy = deepcopy(img)
+    kx,ky = gen_kernels()
+    for i in range(len(img)-2):
+        for j in range(len(img[i])-2):
+            s = 0
+            t = 0
+            for m in range(len(kx)):
+                for n in range(len(kx[m])):
+                    s += dx[i+m][j+n] * kx[m][n]
+                    t += dy[i+m][j+n] * ky[m][n]
+            dx[i][j] = abs(s/9)
+            dy[i][j] = abs(t/9)
+    return dx,dy
+
+
 def derivative_xy(img):
     dx = deepcopy(img)
     dy = deepcopy(img)
@@ -38,13 +81,27 @@ def derivative_xy(img):
 
 
 def gradient_magnitude(dx,dy):
-    magn = deepcopy(dx)
-    theta = deepcopy(dy)
+    rho = deepcopy(dx)
+    theta = np.array([[0.0 for y in range(len(dx[0]))] for x in range(len(dx))])
     for i in range(len(dx)):
         for j in range(len(dx[i])):
-            magn[i][j] = math.sqrt(math.pow(dx[i][j],2)+math.pow(dy[i][j],2))
+            rho[i][j] = math.sqrt(math.pow(dx[i][j],2)+math.pow(dy[i][j],2))
             theta[i][j] = math.atan2(dy[i][j],dx[i][j])
-    return magn,theta
+    return rho,theta
+
+
+def non_max_suppress(rho,theta):
+    for i in range(len(theta)):
+        for j in range(len(theta[i])):
+            theta[i][j] = round_angle(theta[i][j])
+
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        for pixel in row:
+            print(pixel,end=' ')
+        print('')
 
 
 def save_img(filepath,pixels):
@@ -61,15 +118,19 @@ def driver():
     img = init_img(filepath)
     smt = gaussian_smooth(img,3)
     dx,dy = derivative_xy(smt)
-    magn,theta = gradient_magnitude(dx,dy)
-
-    save_img('img/valve_dx.png',dx)
-    save_img('img/valve_dy.png',dy)
-    save_img('img/valve_magn.png',magn)
-    save_img('img/valve_theta.png',theta)
+    rho,theta = gradient_magnitude(dx,dy)
+    non_max_suppress(rho,theta)
 
 
+
+    save_img('img/valve_magnitude.png',rho)
+    #save_img('img/valve_theta.png',theta)
 
 
 if __name__ == '__main__':
     driver()
+
+
+
+
+# canny filter
