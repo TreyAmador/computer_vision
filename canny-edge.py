0b2eb530e37488501aeb9054535887d5f471094d
Trey Amador <tmamador@cpp.edu>
2017-10-21 05:18:48 -0700

    update non max canny

--- canny-edge.py
+++ canny-edge.py
@@ -33,92 +33,92 @@ def new_pixels(img):
 def new_gradient(img):
     return np.zeros(shape=(len(img),len(img[0])))
 
+def new_tuple(img):
+	return np.zeros(shape=(len(img),len(img[0])),dtype='2int8')
 
-def gen_kernels():
+
+def init_kernels():
 	kx = [[-1,0,1],[-2,0,2],[-1,0,1]]
 	ky = [[1,2,1],[0,0,0],[-1,-2,-1]]
 	return kx,ky
 
 
+def round_angle(rad):
+    deg = degrees(rad)
+    if -22.5 <= deg <= 22.5:
+        return 0,1
+    elif 22.5 < deg <= 67.5:
+        return 1,1
+    elif -67.5 <= deg < -22.5:
+        return 1,-1
+    elif 67.5 < deg <= 112.5:
+        return 1,0
+    elif -112.5 <= deg < -67.5:
+        return 1,0
+    elif 112.5 < deg <= 157.5:
+        return 1,-1
+    elif -157.5 <= deg < -112.5:
+        return 1,0
+    elif 157.5 < deg <= 180.0:
+        return 0,1
+    elif -180.0 <= deg < -157.5:
+        return 1,1
+    else:
+        return 0,1
+
+
 def gaussian_blur(img):
-	gauss = np.array(img,dtype=np.float)
-	return cv2.GaussianBlur(gauss,(3,3),0)
+	return cv2.GaussianBlur(img,(3,3),0)
 
 
 def sobel_edge(img):
 	dx = new_gradient(img)
 	dy = new_gradient(img)
-	kx,ky = gen_kernels()
+	kx,ky = init_kernels()
 	for i in range(1,len(img)-1):
 		for j in range(1,len(img[i])-1):
-			s,t = 0.0,0.0
 			for m in range(len(kx)):
 				for n in range(len(kx[m])):
-					s += img[i+m-1][j+n-1] * kx[m][n]
-					t += img[i+m-1][j+n-1] * ky[m][n]
-			dx[i][j] = s
-			dy[i][j] = t
+					dx[i][j] += img[i+m-1][j+n-1] * kx[m][n]
+					dy[i][j] += img[i+m-1][j+n-1] * ky[m][n]
 	return dx,dy
 
 
-def gradient_magnitude(im3h,im3v):
-	grad = np.power(np.power(im3h, 2.0) + np.power(im3v, 2.0), 0.5)
-	theta = np.arctan2(im3v, im3h)
-	thetaQ = (np.round(theta * (5.0 / np.pi)) + 5) % 5 #Quantize direction
-	return grad,theta,thetaQ
+def gradient_magnitude(dx,dy):
+	grad = np.power(np.power(dx,2.0)+np.power(dy,2.0),0.5)
+	theta = np.arctan2(dy,dx)
+	thetaQ = (np.round(theta * (5.0 / np.pi)) + 5) % 5
+	thetaXY = new_tuple(theta)
+	for i in range(len(theta)):
+		for j in range(len(theta[i])):
+			thetaXY[i][j] = round_angle(theta[i][j])
+	return grad,theta,thetaQ,thetaXY
 
 
-def non_max_suppress(im,grad):
-	gradSup = grad.copy()
-	for r in range(im.shape[0]):
-		for c in range(im.shape[1]):
-			#Suppress pixels at the image edge
-			if r == 0 or r == im.shape[0]-1 or c == 0 or c == im.shape[1] - 1:
-				gradSup[r, c] = 0
-				continue
-			tq = thetaQ[r, c] % 4
-
-			if tq == 0: #0 is E-W (horizontal)
-				if grad[r, c] <= grad[r, c-1] or grad[r, c] <= grad[r, c+1]:
-					gradSup[r, c] = 0
-			if tq == 1: #1 is NE-SW
-				if grad[r, c] <= grad[r-1, c+1] or grad[r, c] <= grad[r+1, c-1]:
-					gradSup[r, c] = 0
-			if tq == 2: #2 is N-S (vertical)
-				if grad[r, c] <= grad[r-1, c] or grad[r, c] <= grad[r+1, c]:
-					gradSup[r, c] = 0
-			if tq == 3: #3 is NW-SE
-				if grad[r, c] <= grad[r-1, c-1] or grad[r, c] <= grad[r+1, c+1]:
-					gradSup[r, c] = 0
-	return gradSup
+def non_max_suppress(im,grad,thetaQ,thetaXY):
+	rho = grad.copy()
+	for r in range(1,len(grad)-1):
+		for c in range(1,len(grad[r])-1):
+			i,j = thetaXY[r][c]
+			if grad[r][c] <= grad[r+i][c-j] or grad[r][c] <= grad[r-i][c+j]:
+				rho[r][c] = 0
+	return rho
 
 
 def canny_edge_detector(im,gradSup,theta,thetaQ, blur = 1, highThreshold = 91, lowThreshold = 31):
-
-	#Double threshold
 	strongEdges = (gradSup > highThreshold)
-
-	#Strong has value 2, weak has value 1
 	thresholdedEdges = np.array(strongEdges, dtype=np.uint8) + (gradSup > lowThreshold)
-
-	#Tracing edges with hysteresis
-	#Find weak edge pixels near strong edge pixels
 	finalEdges = strongEdges.copy()
 	currentPixels = []
 	for r in range(1, im.shape[0]-1):
 		for c in range(1, im.shape[1]-1):
 			if thresholdedEdges[r, c] != 1:
-				continue #Not a weak pixel
-
-			#Get 3x3 patch
+				continue
 			localPatch = thresholdedEdges[r-1:r+2,c-1:c+2]
 			patchMax = localPatch.max()
 			if patchMax == 2:
 				currentPixels.append((r, c))
 				finalEdges[r, c] = 1
-
-
-	#Extend strong edges based on current pixels
 	while len(currentPixels) > 0:
 		newPix = []
 		for r, c in currentPixels:
@@ -128,12 +128,9 @@ def canny_edge_detector(im,gradSup,theta,thetaQ, blur = 1, highThreshold = 91, l
 					r2 = r+dr
 					c2 = c+dc
 					if thresholdedEdges[r2, c2] == 1 and finalEdges[r2, c2] == 0:
-						#Copy this weak pixel to final result
 						newPix.append((r2, c2))
 						finalEdges[r2, c2] = 1
 		currentPixels = newPix
-
-
 	return finalEdges
 
 
@@ -145,20 +142,15 @@ def convert_edges(edge):
 	return rho
 
 
-
 if __name__ == '__main__':
 	img = init_img('img/valve.png')
 	img2 = gaussian_blur(img)
 	dx,dy = sobel_edge(img2)
-	grad,theta,thetaQ = gradient_magnitude(dx,dy)
-	sup = non_max_suppress(img,grad)
+	grad,theta,thetaQ,thetaXY = gradient_magnitude(dx,dy)
+	sup = non_max_suppress(img,grad,thetaQ,thetaXY)
 	edges = canny_edge_detector(img,sup,theta,thetaQ)
 	rho = convert_edges(edges)
 	save_img('img/valve_final.png',rho)
-
-
-
-
 
 
 
