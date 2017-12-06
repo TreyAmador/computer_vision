e2166e2cabec4759cfdb138c3b5f6e2bdc49a2df
Trey Amador <tmamador@cpp.edu>
2017-12-06 03:40:10 -0800

    working background removal

--- background.py
+++ background.py
@@ -5,6 +5,12 @@ import numpy as np
 import cv2
 
 
+
+def out_path(filepath,suffix):
+    fp = filepath.split('.')
+    return fp[0]+suffix+'.'+fp[1]
+
+
 def remove_foreground(img):
     hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     lower_green = np.array([50,150,50])
@@ -20,12 +26,36 @@ def threshold(img):
     return thresh
 
 
-frgd = cv2.imread('bgimg/img1_bg1.jpg')
-mask = remove_foreground(frgd)
-thresh = threshold(mask)
+def background_removal(filepath):
+    frgd = cv2.imread(filepath)
+    mask = remove_foreground(frgd)
+    thresh = threshold(mask)
+    rest = img_restore(frgd,thresh)
+    return thresh, rest
 
 
-cv2.imwrite('bgimg/img1_thresh_bg1.jpg',thresh)
+def img_restore(img,mask):
+    gray = cv2.cvtColor(mask,cv2.COLOR_GRAY2BGR)
+    return img*(gray/255)
+
+
+def process_img(filepath):
+    mask,rest = background_removal(filepath)
+    cv2.imwrite(out_path(filepath,'_mask'),mask)
+    cv2.imwrite(out_path(filepath,'_remove'),rest)
+
+
+def driver():
+    process_img('bgimg/img1_bg1.jpg')
+    process_img('bgimg/img2_bg1.jpg')
+    process_img('bgimg/img3_bg2.jpg')
+    process_img('bgimg/img4_bg2.jpg')
+
+
+if __name__ == '__main__':
+    driver()
+
+
 
 
 
