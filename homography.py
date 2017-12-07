8161e4063ddc9e6487c4b9dea9df4a03cce3bf8c
Trey Amador <tmamador@cpp.edu>
2017-12-06 16:56:58 -0800

    hough lines working

--- homography.py
+++ homography.py
@@ -13,53 +13,10 @@ def show_img(img):
 	cv2.destroyAllWindows()
 
 
-
-
-'''
-def hough_lines(img,mask):
-	#gray = cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
-	gray = mask
-	edges = cv2.Canny(gray,50,150,apertureSize = 3)
-	#show_img(edges)
-	#show_img(edges)
-	lines = cv2.HoughLines(edges,1,np.pi/180,200)
-	for rho,theta in lines[0]:
-	    a = np.cos(theta)
-	    b = np.sin(theta)
-	    x0 = a*rho
-	    y0 = b*rho
-	    x1 = int(x0 + 1000*(-b))
-	    y1 = int(y0 + 1000*(a))
-	    x2 = int(x0 - 1000*(-b))
-	    y2 = int(y0 - 1000*(a))
-
-	    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-
-	#cv2.imwrite('houghlines3.jpg',img)
-	show_img(img)
-'''
-
-
-'''
-def filter_img(img):
-	hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-	#lower_green = np.array([50,150,50])
-	#upper_green = np.array([100,255,150])
-	lower_green = np.array([70,140,50])
-	upper_green = np.array([100,255,150])
-	mask = cv2.inRange(hsv,lower_green,upper_green)
-	kernel = np.ones((3,3),np.uint8)
-	#dilate = cv2.dilate(mask,kernel,iterations=3)
-	#dilate = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernel)
-	res = cv2.bitwise_or(img,img,mask=mask)
-	return res
-'''
-
-
 def filter_img(img):
 	fltr = img.copy()
-	lower_green = np.array([60,60,0])
-	upper_green = np.array([255,255,35])
+	lower_green = np.array([63,64,0])
+	upper_green = np.array([255,255,33])
 	mask = cv2.inRange(fltr,lower_green,upper_green)
 	kernel = np.ones((3,3),np.uint8)
 	opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
@@ -72,11 +29,11 @@ def hough_lines(img,mask):
 	#img = cv2.imread('dave.jpg')
 	#gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 	gray = mask
-	edges = cv2.Canny(gray,50,100,apertureSize = 3)
+	edges = cv2.Canny(gray,150,200,apertureSize = 3)
 
 	show_img(edges)
 
-	lines = cv2.HoughLines(edges,1,np.pi/180,200)
+	lines = cv2.HoughLines(edges,1,np.pi/90,110)
 
 	for line in lines:
 		for rho,theta in line:
@@ -89,7 +46,7 @@ def hough_lines(img,mask):
 		    x2 = int(x0 - 1000*(-b))
 		    y2 = int(y0 - 1000*(a))
 
-		    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+		    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
 
 	#cv2.imwrite('houghlines3.jpg',img)
 
